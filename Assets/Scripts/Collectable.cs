11bbcdad7c6aea03592c0a203119d28876e84560
PerseusParadox <44103156+PerseusParadox@users.noreply.github.com>
2018-10-16 15:47:19 +0200

    Fixed fuckup and added collectables
    added collectables
    added holo lights

---
+++ Assets/Scripts/Collectable.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collectable : MonoBehaviour {
+    public float speed;
+    public GameObject particles;
+    public GameObject sound;
+    void Start () {
+    }
+
+    void Update () {
+        transform.Translate (Vector3.left * speed * Time.deltaTime , Space.World);
+        if ( transform.position.x < -60f ) {
+            Destroy (this.gameObject);
+        }
+    }
+    private void OnTriggerEnter (Collider other) {
+        if ( other.tag == "Player" ) {
+            Debug.Log ("Collider works");
+            Instantiate (particles , transform.position , Quaternion.identity);
+            Instantiate (sound , transform.position , Quaternion.identity);
+
+            Destroy (this.gameObject);
+        }
+    }
+}
