ac175e700e2bda72d1fcf777bd49319d000f7afd
PerseusParadox <44103156+PerseusParadox@users.noreply.github.com>
2018-10-20 22:51:00 +0200

    Added background
    animated it
    optimized the ps and sounds a little made them deslf destroy in 2 secs.
    need to clean up and make another level

---
+++ Assets/Prefabs/DestroyNSecScript.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyNSecScript : MonoBehaviour {
+    public float secs;
+    void Update () {
+        Destroy (this.gameObject , secs);
+    }
+}
